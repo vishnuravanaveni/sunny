1fb50de8ded2551ba343b6169b81375d54419f5c
vishnuravanaveni <vishnuh.ravan@gmail.com>
2017-06-10 14:42:04 -0500

    vishnus commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Hello Please enter your First Name:");
+        String FirstName = Console.ReadLine();
+        Console.WriteLine("Hello {0}, please enter you Lastname", FirstName);
+        String LastName = Console.ReadLine();
+        Console.WriteLine("Hello {0} {1}", FirstName, LastName);
+        Console.ReadLine();
+
+
+    }
+    }
